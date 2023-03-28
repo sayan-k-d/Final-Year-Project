d97a904fd4f2a8d9731e5f2a8fde43de45ff1544
sayan-k-d <sayan.sayanak2000@gmail.com>
2023-03-29 00:22:06 +0530

    Filteration Added to getAllUsers API, now based on User Type Data Can be Fetched.

--- graphql/resolvers/users/queries.js
+++ graphql/resolvers/users/queries.js
@@ -1,27 +1,36 @@
+import { AuthenticationError } from "apollo-server";
 import { Admin, SuperAdmin, Surveyor, User } from "../../../db/models/index.js";
 
 const adminQueries = {
-  getAllUsers: async () => {
-    const allAdmins = await User.find({ userType: "ADMIN" });
-    return allAdmins.map(async ({ _doc }) => ({
-      ..._doc,
-      userDetails: await Admin.findById(_doc.referenceId),
-    }));
+  getAllUsers: async (_, { UserType }, { currentUser }) => {
+    if (currentUser) {
+      const allAdmins = await User.find({ userType: UserType });
+      return allAdmins.map(async ({ _doc }) => ({
+        ..._doc,
+        userDetails: await Admin.findById(_doc.referenceId),
+      }));
+    } else {
+      return new AuthenticationError();
+    }
   },
-  getUserById: async (_, { id }) => {
-    const user = await User.findById(id);
-    let specificUser;
-    if (user.userType === "ADMIN") {
-      specificUser = await Admin.findById(user.referenceId);
+  getUserById: async (_, { id }, { currentUser }) => {
+    if (currentUser) {
+      const user = await User.findById(id);
+      let specificUser;
+      if (user.userType === "ADMIN") {
+        specificUser = await Admin.findById(user.referenceId);
+      }
+      if (user.userType == "SUPER_ADMIN") {
+        specificUser = await SuperAdmin.findById(user.referenceId);
+      }
+      if (user.userType === "SURVEYOR") {
+        specificUser = await Surveyor.findById(user.referenceId);
+      }
+      user.userDetails = specificUser;
+      return user;
+    } else {
+      return new AuthenticationError();
     }
-    if (user.userType == "SUPER_ADMIN") {
-      specificUser = await SuperAdmin.findById(user.referenceId);
-    }
-    if (user.userType === "SURVEYOR") {
-      specificUser = await Surveyor.findById(user.referenceId);
-    }
-    user.userDetails = specificUser;
-    return user;
   },
 };
 export default adminQueries;
